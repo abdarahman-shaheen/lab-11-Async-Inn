6357fb5523a063f805385fd0d7918c3b3cf8cec7
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-07-16 11:19:21 +0300

    Add project files.

---
+++ lab-11-Async-Inn/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab_11_Async_Inn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
